a58d1c22276b2d1993d6a710c409eb976e7235fd
Rob Geada <rob@geada.net>
2023-09-25 17:47:01 +0100

    Trustyai CI debugging

--- tests/basictests/trustyai.sh
+++ tests/basictests/trustyai.sh
@@ -48,6 +48,8 @@ function check_trustyai_resources() {
 
   os::cmd::try_until_text "oc get deployment trustyai-service" "trustyai-service" $odhdefaulttimeout $odhdefaultinterval || eval "$FAILURE_HANDLING"
   os::cmd::try_until_text "oc get route trustyai-service-route" "trustyai-service-route" $odhdefaulttimeout $odhdefaultinterval || eval "$FAILURE_HANDLING"
+  echo $(oc get pods)
+  echo $(oc get events | grep trustyai-service)
   os::cmd::try_until_text "oc get pod | grep trustyai-service" "1/1" $odhdefaulttimeout $odhdefaultinterval || eval "$FAILURE_HANDLING"
 
 }
